2fa14e55be3c906c9dda5ad7d7f8143259bc4d3e
VektirLabs <tyler@vektirlabs.com>
2022-12-07 16:28:39 -0600

    UPDATE SIDEBAR LOGO

--- components/sidebar.py
+++ components/sidebar.py
@@ -5,9 +5,9 @@ import plotly.express as px
 import pandas as pd
 
 # Globals ----------------------------------------------------------------------
-VEKTIR_LOGO = "https://github.com/VektirLabs/Dash_Template_App/blob/v1.0/assets/img/Website_Color_Logo.png"
-VEKTIR_TEXT = "https://storage.cloud.google.com/anvil_works_bucket/Logo-Text-Only.png"
-VEKTIR_COM = "https://vektirlabs.com"
+VEKTIR_LABS_LOGO = "https://github.com/VektirLabs/Dash_Template_App/blob/v1.0/assets/img/Website_Color_Logo.png"
+VEKTIR_LABS_LOGO_TEXT = "https://storage.cloud.google.com/anvil_works_bucket/Logo-Text-Only.png"
+VEKTIVEKTIR_LABS_COMR_COM = "https://vektirlabs.com"
 
 
 # Sidebar ----------------|
@@ -25,11 +25,11 @@ SIDEBAR_STYLE = {
 def get_sidebar():
     return html.Div([
                 html.A(
-                    href=VEKTIR_COM,
+                    href=VEKTIR_LABS_COM,
                     children=[
                         html.Img(
                             alt="VektirLabs.com",
-                            src=VEKTIR_TEXT,
+                            src=app.get_asset_url(VEKTIR_LABS_LOGO_TEXT),
                             height="60px"
                         )
                     ]
